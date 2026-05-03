04fa842cc48292dbbeb138e1893f5a18b79d8739
moonjab-dot-com <moonjab.com@gmail.com>
2026-05-02 21:10:20 -0500

    Update skeleton-loader.tsx

--- src/components/ui/skeleton-loader.tsx
+++ src/components/ui/skeleton-loader.tsx
@@ -57,30 +57,4 @@ function SkeletonDashboard() {
   );
 }
 
-function SkeletonOpportunityCard() {
-  return (
-    <div className="rounded-2xl border border-border bg-card p-6 shadow-clovely-md">
-      <div className="space-y-4">
-        <div className="flex items-start justify-between">
-          <div className="space-y-2 flex-1">
-            <Skeleton className="h-6 w-3/4" />
-            <Skeleton className="h-4 w-1/2" />
-          </div>
-          <Skeleton className="h-16 w-16 rounded-xl" />
-        </div>
-        <Skeleton className="h-20 w-full" />
-        <div className="flex gap-2">
-          <Skeleton className="h-6 w-20 rounded-full" />
-          <Skeleton className="h-6 w-24 rounded-full" />
-          <Skeleton className="h-6 w-20 rounded-full" />
-        </div>
-        <div className="flex items-center justify-between pt-4">
-          <Skeleton className="h-8 w-24" />
-          <Skeleton className="h-10 w-32" />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export { Skeleton, SkeletonCard, SkeletonDashboard, SkeletonOpportunityCard };
+export { Skeleton, SkeletonCard, SkeletonDashboard };
