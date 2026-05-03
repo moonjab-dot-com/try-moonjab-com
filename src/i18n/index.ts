6dc861c538bb11c6150edd70de5e36ff6c072c28
moonjab-dot-com <moonjab.com@gmail.com>
2026-05-02 21:16:40 -0500

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,14 +4,10 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import es from './locales/es.json';
 import en from './locales/en.json';
-import pt from './locales/pt.json';
-import fr from './locales/fr.json';
 
 const resources = {
   es: { translation: es },
   en: { translation: en },
-  pt: { translation: pt },
-  fr: { translation: fr },
 };
 
 i18n
@@ -21,6 +17,7 @@ i18n
     resources,
     lng: localStorage.getItem('moonjab_language') || 'es',
     fallbackLng: 'es',
+    supportedLngs: ['es', 'en'],
     debug: false,
     interpolation: {
       escapeValue: false,
