51a32aadca7fbbdd1f46065e82e792265465326d
moonjab-dot-com <moonjab.com@gmail.com>
2026-05-03 19:21:31 -0500

    Update index.ts

--- supabase/functions/check-subscription/index.ts
+++ supabase/functions/check-subscription/index.ts
@@ -18,7 +18,7 @@ serve(async (req) => {
   }
 
   const supabaseClient = createClient(
-    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("VITE_SUPABASE_URL") ?? "",
     Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
     { auth: { persistSession: false } }
   );
